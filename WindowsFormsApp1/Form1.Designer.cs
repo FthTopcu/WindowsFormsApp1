f7fb092c7efc04eae68d1b049d5a449801db2ab5
FthTopcu <fthtpu@gmail.com>
2021-12-07 21:19:05 +0300

    2

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Bir = new System.Windows.Forms.Button();
+            this.Iki = new System.Windows.Forms.Button();
+            this.Uc = new System.Windows.Forms.Button();
+            this.Alti = new System.Windows.Forms.Button();
+            this.Bes = new System.Windows.Forms.Button();
+            this.Dort = new System.Windows.Forms.Button();
+            this.Dokuz = new System.Windows.Forms.Button();
+            this.Sekiz = new System.Windows.Forms.Button();
+            this.Yedi = new System.Windows.Forms.Button();
+            this.Carpma = new System.Windows.Forms.Button();
+            this.Sifir = new System.Windows.Forms.Button();
+            this.Bolme = new System.Windows.Forms.Button();
+            this.Ce = new System.Windows.Forms.Button();
+            this.Cikarma = new System.Windows.Forms.Button();
+            this.Toplama = new System.Windows.Forms.Button();
+            this.Sonuc = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 400);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Bir
+            // 
+            this.Bir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Bir.Location = new System.Drawing.Point(439, 118);
+            this.Bir.Name = "Bir";
+            this.Bir.Size = new System.Drawing.Size(75, 23);
+            this.Bir.TabIndex = 2;
+            this.Bir.Text = "1";
+            this.Bir.UseVisualStyleBackColor = true;
+            this.Bir.Click += new System.EventHandler(this.Bir_Click);
+            // 
+            // Iki
+            // 
+            this.Iki.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Iki.Location = new System.Drawing.Point(520, 118);
+            this.Iki.Name = "Iki";
+            this.Iki.Size = new System.Drawing.Size(75, 23);
+            this.Iki.TabIndex = 3;
+            this.Iki.Text = "button3";
+            this.Iki.UseVisualStyleBackColor = true;
+            this.Iki.Click += new System.EventHandler(this.Iki_Click);
+            // 
+            // Uc
+            // 
+            this.Uc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Uc.Location = new System.Drawing.Point(601, 118);
+            this.Uc.Name = "Uc";
+            this.Uc.Size = new System.Drawing.Size(75, 23);
+            this.Uc.TabIndex = 4;
+            this.Uc.Text = "button4";
+            this.Uc.UseVisualStyleBackColor = true;
+            this.Uc.Click += new System.EventHandler(this.Uc_Click);
+            // 
+            // Alti
+            // 
+            this.Alti.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Alti.Location = new System.Drawing.Point(601, 147);
+            this.Alti.Name = "Alti";
+            this.Alti.Size = new System.Drawing.Size(75, 23);
+            this.Alti.TabIndex = 7;
+            this.Alti.Text = "button5";
+            this.Alti.UseVisualStyleBackColor = true;
+            this.Alti.Click += new System.EventHandler(this.Alti_Click);
+            // 
+            // Bes
+            // 
+            this.Bes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Bes.Location = new System.Drawing.Point(520, 147);
+            this.Bes.Name = "Bes";
+            this.Bes.Size = new System.Drawing.Size(75, 23);
+            this.Bes.TabIndex = 6;
+            this.Bes.Text = "button6";
+            this.Bes.UseVisualStyleBackColor = true;
+            this.Bes.Click += new System.EventHandler(this.Bes_Click);
+            // 
+            // Dort
+            // 
+            this.Dort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Dort.Location = new System.Drawing.Point(439, 147);
+            this.Dort.Name = "Dort";
+            this.Dort.Size = new System.Drawing.Size(75, 23);
+            this.Dort.TabIndex = 5;
+            this.Dort.Text = "button7";
+            this.Dort.UseVisualStyleBackColor = true;
+            this.Dort.Click += new System.EventHandler(this.Dort_Click);
+            // 
+            // Dokuz
+            // 
+            this.Dokuz.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Dokuz.Location = new System.Drawing.Point(601, 176);
+            this.Dokuz.Name = "Dokuz";
+            this.Dokuz.Size = new System.Drawing.Size(75, 23);
+            this.Dokuz.TabIndex = 10;
+            this.Dokuz.Text = "button8";
+            this.Dokuz.UseVisualStyleBackColor = true;
+            this.Dokuz.Click += new System.EventHandler(this.Dokuz_Click);
+            // 
+            // Sekiz
+            // 
+            this.Sekiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sekiz.Location = new System.Drawing.Point(520, 176);
+            this.Sekiz.Name = "Sekiz";
+            this.Sekiz.Size = new System.Drawing.Size(75, 23);
+            this.Sekiz.TabIndex = 9;
+            this.Sekiz.Text = "button9";
+            this.Sekiz.UseVisualStyleBackColor = true;
+            this.Sekiz.Click += new System.EventHandler(this.Sekiz_Click);
+            // 
+            // Yedi
+            // 
+            this.Yedi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Yedi.Location = new System.Drawing.Point(439, 176);
+            this.Yedi.Name = "Yedi";
+            this.Yedi.Size = new System.Drawing.Size(75, 23);
+            this.Yedi.TabIndex = 8;
+            this.Yedi.Text = "button10";
+            this.Yedi.UseVisualStyleBackColor = true;
+            this.Yedi.Click += new System.EventHandler(this.Yedi_Click);
+            // 
+            // Carpma
+            // 
+            this.Carpma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Carpma.Location = new System.Drawing.Point(601, 205);
+            this.Carpma.Name = "Carpma";
+            this.Carpma.Size = new System.Drawing.Size(75, 23);
+            this.Carpma.TabIndex = 13;
+            this.Carpma.Text = "button11";
+            this.Carpma.UseVisualStyleBackColor = true;
+            this.Carpma.Click += new System.EventHandler(this.Carpma_Click);
+            // 
+            // Sifir
+            // 
+            this.Sifir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sifir.Location = new System.Drawing.Point(520, 205);
+            this.Sifir.Name = "Sifir";
+            this.Sifir.Size = new System.Drawing.Size(75, 23);
+            this.Sifir.TabIndex = 12;
+            this.Sifir.Text = "button12";
+            this.Sifir.UseVisualStyleBackColor = true;
+            this.Sifir.Click += new System.EventHandler(this.Sifir_Click);
+            // 
+            // Bolme
+            // 
+            this.Bolme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Bolme.Location = new System.Drawing.Point(439, 205);
+            this.Bolme.Name = "Bolme";
+            this.Bolme.Size = new System.Drawing.Size(75, 23);
+            this.Bolme.TabIndex = 11;
+            this.Bolme.Text = "button13";
+            this.Bolme.UseVisualStyleBackColor = true;
+            this.Bolme.Click += new System.EventHandler(this.Bolme_Click);
+            // 
+            // Ce
+            // 
+            this.Ce.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Ce.Location = new System.Drawing.Point(682, 118);
+            this.Ce.Name = "Ce";
+            this.Ce.Size = new System.Drawing.Size(75, 23);
+            this.Ce.TabIndex = 16;
+            this.Ce.Text = "button14";
+            this.Ce.UseVisualStyleBackColor = true;
+            this.Ce.Click += new System.EventHandler(this.Ce_Click);
+            // 
+            // Cikarma
+            // 
+            this.Cikarma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Cikarma.Location = new System.Drawing.Point(683, 147);
+            this.Cikarma.Name = "Cikarma";
+            this.Cikarma.Size = new System.Drawing.Size(75, 23);
+            this.Cikarma.TabIndex = 15;
+            this.Cikarma.Text = "button15";
+            this.Cikarma.UseVisualStyleBackColor = true;
+            this.Cikarma.Click += new System.EventHandler(this.Cikarma_Click);
+            // 
+            // Toplama
+            // 
+            this.Toplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Toplama.Location = new System.Drawing.Point(683, 176);
+            this.Toplama.Name = "Toplama";
+            this.Toplama.Size = new System.Drawing.Size(75, 23);
+            this.Toplama.TabIndex = 14;
+            this.Toplama.Text = "button16";
+            this.Toplama.UseVisualStyleBackColor = true;
+            this.Toplama.Click += new System.EventHandler(this.Toplama_Click);
+            // 
+            // Sonuc
+            // 
+            this.Sonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sonuc.Location = new System.Drawing.Point(682, 205);
+            this.Sonuc.Name = "Sonuc";
+            this.Sonuc.Size = new System.Drawing.Size(75, 23);
+            this.Sonuc.TabIndex = 17;
+            this.Sonuc.Text = "button17";
+            this.Sonuc.UseVisualStyleBackColor = true;
+            this.Sonuc.Click += new System.EventHandler(this.Sonuc_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(439, 77);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 35);
+            this.textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 538);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sonuc);
+            this.Controls.Add(this.Ce);
+            this.Controls.Add(this.Cikarma);
+            this.Controls.Add(this.Toplama);
+            this.Controls.Add(this.Carpma);
+            this.Controls.Add(this.Sifir);
+            this.Controls.Add(this.Bolme);
+            this.Controls.Add(this.Dokuz);
+            this.Controls.Add(this.Sekiz);
+            this.Controls.Add(this.Yedi);
+            this.Controls.Add(this.Alti);
+            this.Controls.Add(this.Bes);
+            this.Controls.Add(this.Dort);
+            this.Controls.Add(this.Uc);
+            this.Controls.Add(this.Iki);
+            this.Controls.Add(this.Bir);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Bir;
+        private System.Windows.Forms.Button Iki;
+        private System.Windows.Forms.Button Uc;
+        private System.Windows.Forms.Button Alti;
+        private System.Windows.Forms.Button Bes;
+        private System.Windows.Forms.Button Dort;
+        private System.Windows.Forms.Button Dokuz;
+        private System.Windows.Forms.Button Sekiz;
+        private System.Windows.Forms.Button Yedi;
+        private System.Windows.Forms.Button Carpma;
+        private System.Windows.Forms.Button Sifir;
+        private System.Windows.Forms.Button Bolme;
+        private System.Windows.Forms.Button Ce;
+        private System.Windows.Forms.Button Cikarma;
+        private System.Windows.Forms.Button Toplama;
+        private System.Windows.Forms.Button Sonuc;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double FirstNumber;
+
+        string Operation;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Button b = new Button();
+            //this.Controls.Add(b);
+            //b.Width = 50;
+            //b.Height = 50;
+            //b.Left = 50;
+            //b.Top = 50;
+
+            Button[,] button = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+            this.Controls.Add(button[0, 0]);
+            for (int i = 0; i < button.GetLength(0); i++)
+            {
+                for (int j = 0; j < button.GetLength(1); j++)
+                {
+                    button[i, j] = new Button();
+                    button[i, j].Width = 50;
+                    button[i, j].Height = 50;
+                    button[i, j].Left = left;
+                    button[i, j].Top = top;
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+
+                        button[i, j].BackColor = Color.Black;
+                        button[i, j].Click += new EventHandler(buttonSiyah_Click);
+
+                    }
+                    else
+                    {
+                        button[i, j].BackColor = Color.White;
+                        button[i, j].Click += new EventHandler(buttonBeyaz_Click);
+                    }
+                    groupBox1.Controls.Add(button[i, j]);
+                    groupBox1.Name = "Kareler";
+
+                }
+                left = 0;
+                top += 50;
+            }
+        }
+        private void buttonSiyah_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Siyah");
+        }
+        private void buttonBeyaz_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Beyaz");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Sifir.Text = "0";
+            Bir.Text = "1";
+            Iki.Text = "2";
+            Uc.Text = "3";
+            Dort.Text = "4";
+            Bes.Text = "5";
+            Alti.Text = "6";
+            Yedi.Text = "7";
+            Sekiz.Text = "8";
+            Dokuz.Text = "9";
+            Ce.Text = "Ce";
+            Cikarma.Text = "-";
+            Toplama.Text = "+";
+            Carpma.Text = "*";
+            Bolme.Text = "/";
+            Sonuc.Text = "=";
+
+        }
+
+        private void Bir_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "1";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "1";
+
+            }
+        }
+
+        private void Iki_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "2";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "2";
+
+            }
+        }
+
+        private void Uc_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "3";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "3";
+
+            }
+        }
+
+        private void Dort_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "4";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "4";
+
+            }
+        }
+
+        private void Bes_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "5";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "5";
+
+            }
+        }
+
+        private void Alti_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "6";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "6";
+
+            }
+        }
+
+        private void Yedi_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "7";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "7";
+
+            }
+        }
+
+        private void Sekiz_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "8";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "8";
+
+            }
+        }
+
+        private void Dokuz_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "9";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "9";
+
+            }
+        }
+
+        private void Sifir_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text == null)
+
+            {
+
+                textBox1.Text = "0";
+
+            }
+
+            else
+
+            {
+
+                textBox1.Text = textBox1.Text + "0";
+
+            }
+        }
+
+        private void Ce_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            
+        }
+
+        private void Cikarma_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = null;
+            Operation = "-";
+        }
+
+        private void Toplama_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = null;
+            Operation = "+";
+        }
+
+        private void Bolme_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = null;
+            Operation = "/";
+        }
+
+        private void Carpma_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = null;
+            Operation = "*";
+        }
+
+        private void Sonuc_Click(object sender, EventArgs e)
+        {
+            double SecondNumber;
+
+            double Result;
+            try
+            {
+                SecondNumber = Convert.ToDouble(textBox1.Text);
+
+            
+            
+            if (Operation == "+")
+
+            {
+
+                Result = (FirstNumber + SecondNumber);
+
+                textBox1.Text = Convert.ToString(Result);
+
+                FirstNumber = Result;
+
+            }
+
+            if (Operation == "-")
+
+            {
+
+                Result = (FirstNumber - SecondNumber);
+
+                textBox1.Text = Convert.ToString(Result);
+
+                FirstNumber = Result;
+
+
+
+            }
+
+            if (Operation == "*")
+
+            {
+
+                Result = (FirstNumber * SecondNumber);
+
+                textBox1.Text = Convert.ToString(Result);
+
+                FirstNumber = Result;
+
+            }
+
+            if (Operation == "/")
+            {
+                if (SecondNumber == 0)
+
+                {
+
+                    textBox1.Text = "Uygulanamaz";
+
+                }
+
+                else
+
+                {
+
+                    Result = (FirstNumber / SecondNumber);
+
+                    textBox1.Text = Convert.ToString(Result);
+
+                    FirstNumber = Result;
+                }
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
